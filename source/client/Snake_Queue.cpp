4a49529e8cba43b8927595bba475aa31e63d1d17
nadacant <nadacant@iupui.edu>
2018-04-24 19:47:36 -0400

    Rename SnakeQueue file

--- source/client/Snake_Queue.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * Snake_Queue.cpp:
- * 	Implementation file for the snake tail piece queue.
- */
-
-#include "SnakeQueue.h"
-
-// Initialize the queue's parameters
-void init_queue(Snake_Queue* 
