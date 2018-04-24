dc45bcb5ff180ee6d85fb4cd124df2082c55967f
nadacant <nadacant@iupui.edu>
2018-04-24 19:18:57 -0400

    Add functions to snake queue

--- include/SnakeQueue.h
+++ include/SnakeQueue.h
@@ -6,10 +6,13 @@
 
 #define QUEUE_LEN 3600
 
+// Snake_Queue: to enqueue/dequeue snake position history
 typedef struct {
-	Tail queue_arr[QUEUE_LEN];
-	int head, tail;
+	Tail queue_arr[QUEUE_LEN]; 	// Actual data container
+	int head, tail; 			// "Pointers" to the beginning/end
+	int length; 				// Positional difference between head/tail
 } Snake_Queue
 
 void init_queue(Snake_Queue& the_queue);
 void enqueue(Snake_Queue& the_queue, item);
+void dequeue(Snake_Queue& the_queue, item);
