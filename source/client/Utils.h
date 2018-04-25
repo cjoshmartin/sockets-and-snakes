ea716301680d316b09098c9e5aaccd359ef8d1ee
nadacant <nadacant@iupui.edu>
2018-04-25 14:49:37 -0400

    Add Utils header for set_ticker and test set_ticker

---
+++ source/client/Utils.h
@@ -0,0 +1,10 @@
+/*
+ * Utility functions to control certain Unix components.
+ */
+
+#ifndef _UTILS_H
+#define _UTILS_H
+
+int set_ticker( int n_msecs );
+
+#endif
--- source/client/main.cpp
+++ source/client/main.cpp
@@ -3,6 +3,7 @@
  *  supporting socket communication with the server.
  */
 
+#include "Utils.h"
 #include "SnakeBoard.h"
 #include <curses.h>
 #include <iostream>
@@ -14,10 +15,10 @@
 // Global update flag to control the timing of the system. Set to "true"
 //  by the alarm handler so that refresh does not occur more often than
 //  once every 0.1 seconds.
-bool updateFlag = true;
+bool drawFlag = true;
 
 void handle(int signum);
-//void updateHandler(int signum);
+void alarmHandle(int signum);
 void UpdateBoard(BoardState& theBoard);
 
 int main(void) {
@@ -26,14 +27,24 @@ int main(void) {
 	// Initialize players based on comms (which player I am, 1 or 2)
 	// Set up alarm and stuff
 	// Wait for input and update position accordingly
+
+	// Install signal handlers: ^C and alarm
 	signal(SIGINT, handle);
+	signal(SIGALRM, alarmHandle);
+
+	// Initialize board and current player according to initial communication
 	SnakeBoard myBoard;
 	myBoard.setCurrentPlayer(1);
 	BoardState newBoard;
 	int i = 20;
+
+	// Set refresh rate
+	set_ticker(10);
+
 	while (i) {
 		UpdateBoard(newBoard);
 		myBoard.update(newBoard);
+		while (!drawFlag) ;
 		myBoard.draw();
 		newBoard = myBoard.collectInput(newBoard);
 	}
@@ -44,7 +55,13 @@ int main(void) {
 // Gets a new board state by socket communiation
 void UpdateBoard(BoardState& theBoard) {
 	theBoard.update();
-	usleep(100000);
+	drawFlag = false;
+}
+
+// Sets the drawFlag to enforce a minimum refresh time
+void alarmHandle(int signum) {
+	signal(SIGALRM, alarmHandle);
+	drawFlag = true;
 }
 
 // Control-c handler
